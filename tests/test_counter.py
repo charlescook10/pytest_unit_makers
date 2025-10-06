01d2968e4b73d4fece4e37058cb987def180c8b4
Charles Cook <charles.cook80@yahoo.co.uk>
2025-10-06 12:02:18 +0100

    Add counter test, implement, pass

---
+++ tests/test_counter.py
@@ -0,0 +1,30 @@
+from lib.counter import Counter
+
+def test_counter_add_1_to_10_times():
+    counter = Counter()
+
+    for i in range(1,11):
+        counter.add(i)
+    
+    result = counter.report()
+
+    assert result == "Counted to 55 so far."
+
+def test_counter_add_0():
+    counter = Counter()
+
+    counter.add(0)
+
+    result = counter.report()
+
+    assert result == "Counted to 0 so far."
+
+
+def test_counter_add_a_negative():
+    counter = Counter()
+
+    counter.add(-5)
+
+    result = counter.report()
+
+    assert result == "Counted to -5 so far."
